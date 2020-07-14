515f1fb85f99fa65c41a63675e2d585aa21174da
Daniel Ong <crimsonknightd@hotmail.com>
2020-07-15 00:06:07 +0900

    Added Web Components to the game.

---
+++ web/src/main/java/org/example/service/GameServiceImpl.java
@@ -0,0 +1,58 @@
+package org.example.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.Game;
+import org.example.MessageGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Service
+public class GameServiceImpl implements GameService {
+    // == fields ==
+    private final Game game;
+    private final MessageGenerator messageGenerator;
+
+    // == constructors ==
+    @Autowired
+    public GameServiceImpl(Game game, MessageGenerator messageGenerator) {
+        this.game = game;
+        this.messageGenerator = messageGenerator;
+    }
+
+    // == init ==
+    @PostConstruct
+    public void init() {
+        log.info("The number is {}", game.getNumber());
+        log.info("main message is {}", messageGenerator.getMainMessage());
+    }
+
+    // == public methods ==
+    @Override
+    public boolean isGameOver() {
+        return game.isGameLost() || game.isGameWon();
+    }
+
+    @Override
+    public String getMainMessage() {
+        return messageGenerator.getMainMessage();
+    }
+
+    @Override
+    public String getResultMessage() {
+        return messageGenerator.getResultMessage();
+    }
+
+    @Override
+    public void checkGuess(int guess) {
+        game.setGuess(guess);
+        game.check();
+    }
+
+    @Override
+    public void reset() {
+        game.reset();
+    }
+}
